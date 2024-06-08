7c5df2fd2f3d7286e527ff0cb245931f7905a81d
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-08 18:48:01 -0400

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+import { useGlobalContext } from "./context";
+
 const App = () => {
-  return <h2>Strapi Starter</h2>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
 };
 export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+const Navbar = () => {
+  const { openSidebar } = useGlobalContext();
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn" onClick={openSidebar}>
+          <FaBars />
+        </button>
+        {/* nav links later */}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
