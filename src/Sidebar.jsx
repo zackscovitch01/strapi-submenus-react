7af65d6e4cfe9523d9b9537ef45a6764c6ca89d5
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-06-08 21:20:01 -0400

    submenu

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,37 @@
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobalContext } from "./context";
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  const { isSidebar, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <div className="sidebar-links">
+        {sublinks.map((sublink) => {
+          const { pageId, page, links } = sublink;
+          return (
+            <article key={pageId}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map((link) => {
+                  const { url, icon, label, id } = link;
+                  return (
+                    <a key={id} href={url}>
+                      {icon}
+                      {label}
+                    </a>
+                  );
+                })}
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 export default Sidebar;
